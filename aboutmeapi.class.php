5fa48c5d3d98320c971243fef9501549eb0996b6
David Grayston <davidgrayston@googlemail.com>
2012-10-16 22:51:04 +0100

    Fixed tab formatting

--- aboutmeapi.class.php
+++ aboutmeapi.class.php
@@ -1,127 +1,127 @@
 <?php 
 class AboutMeApi
 {
-	/**
-	 * API path format.
-	 */
-	private $apiPathFormat = 'https://api.about.me/api/<version>/<format>/<obj_type>/<action>/<object><query>';
+  /**
+   * API path format.
+   */
+  private $apiPathFormat = 'https://api.about.me/api/<version>/<format>/<obj_type>/<action>/<object><query>';
 
-	/**
-	 * API key set in construct.
-	 */
-	private $key;
+  /**
+   * API key set in construct.
+   */
+  private $key;
 
-	/**
-	 * API version.
-	 */
-	private $version = 'v2';
+  /**
+   * API version.
+   */
+  private $version = 'v2';
 
-	/**
-	 * API format
-	 */
-	private $format = 'json';
-	
-	/**
-	 * CURL timeout.
-	 */
-	private $timeout = 2;
+  /**
+   * API format
+   */
+  private $format = 'json';
 
-	/**
-	 * Construct.
-	 *
-	 * @param $params
-	 * 	'key' (required)
-	 *  'version' (optional)
-	 *  'format' (optional)
-	 *  'timeout' CURL timeout seconds (optional)
-	 * @throws Exception
-	 */
-	public function __construct($params = array()) {
-		// Set key - this is a required parameter
-		if (!isset($params['key'])) {
-			throw new Exception('Please specify an about.me developer key');
-		}
-		$this->key = $params['key'];
+  /**
+   * CURL timeout.
+   */
+  private $timeout = 2;
 
-		// Set version - defaults to 'v2'
-		if (isset($params['version'])) { 
-			$this->version = $params['version'];
-		}
+  /**
+   * Construct.
+   *
+   * @param $params
+   *   'key' (required)
+   *   'version' (optional)
+   *   'format' (optional)
+   *   'timeout' CURL timeout seconds (optional)
+   * @throws Exception
+   */
+  public function __construct($params = array()) {
+    // Set key - this is a required parameter
+    if (!isset($params['key'])) {
+      throw new Exception('Please specify an about.me developer key');
+    }
+    $this->key = $params['key'];
 
-		// Set format - defaults to 'json'
-		if (isset($params['format'])) {
-			$this->format = $params['format'];
-		}
+    // Set version - defaults to 'v2'
+    if (isset($params['version'])) { 
+      $this->version = $params['version'];
+    }
 
-		// Custom timeout.
-		if (isset($params['timeout'])) {
-			$this->timeout = intval($params['timeout']);
-		}
-	}
+    // Set format - defaults to 'json'
+    if (isset($params['format'])) {
+      $this->format = $params['format'];
+    }
 
-	/**
-	 * Fetch remote data.
-	 * 
-	 * @param $obj_type
-	 * @param $action
-	 * @param $object
-	 * @param $queryString
-	 */
-	private function getData($obj_type, $action, $object, $queryArray = array()) 
-	{
-		// Build query string to append to API url.
-		$appendQueryString = !empty($queryArray) ? '?' . http_build_query($queryArray) : '';
-		
-		// API url replacements.
-		$replacements = array(
-			'<version>' => $this->version,
-			'<format>' => $this->format,
-			'<obj_type>' => $obj_type,
-			'<action>' => $action,
-			'<object>' => $object,
-			'<query>' => $appendQueryString
-		);
-		
-		// Populate API url replacements.
-		$url = str_replace(array_keys($replacements), $replacements, $this->apiPathFormat) ;
-		
-		// Initialise curl request.
-		$ch = curl_init($url);
-		
-		// Set authentication headers.
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		  'Authorization: Basic ' . $this->key
-		));
+    // Custom timeout.
+    if (isset($params['timeout'])) {
+      $this->timeout = intval($params['timeout']);
+    }
+  }
 
-		// CURL settings.
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  /**
+   * Fetch remote data.
+   * 
+   * @param $obj_type
+   * @param $action
+   * @param $object
+   * @param $queryString
+   */
+  private function getData($obj_type, $action, $object, $queryArray = array()) 
+  {
+    // Build query string to append to API url.
+    $appendQueryString = !empty($queryArray) ? '?' . http_build_query($queryArray) : '';
+    
+    // API url replacements.
+    $replacements = array(
+      '<version>' => $this->version,
+      '<format>' => $this->format,
+      '<obj_type>' => $obj_type,
+      '<action>' => $action,
+      '<object>' => $object,
+      '<query>' => $appendQueryString
+    );
+    
+    // Populate API url replacements.
+    $url = str_replace(array_keys($replacements), $replacements, $this->apiPathFormat) ;
+    
+    // Initialise curl request.
+    $ch = curl_init($url);
+    
+    // Set authentication headers.
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Authorization: Basic ' . $this->key
+    ));
 
-		// Must be POST request.
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, '');
+    // CURL settings.
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-		// Get JSON response.
-		$response = json_decode(curl_exec($ch));
+    // Must be POST request.
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, '');
 
-		// Close connection.
-		curl_close($ch);
-		
-		return $response;
-	}
+    // Get JSON response.
+    $response = json_decode(curl_exec($ch));
 
-	/**
-	 * Get user profile.
-	 * 
-	 * @param $username
-	 * @param $extended
-	 */
-	public function getProfile($username, $extended = false)
-	{
-		// Build query array - defaults to empty array.
-		$queryArray = $extended ? array('extended' => 'true') : array();
-		// Return API response.
-		return $this->getData('user', 'view', $username, $queryArray);
-	}
+    // Close connection.
+    curl_close($ch);
+    
+    return $response;
+  }
+
+  /**
+   * Get user profile.
+   * 
+   * @param $username
+   * @param $extended
+   */
+  public function getProfile($username, $extended = false)
+  {
+    // Build query array - defaults to empty array.
+    $queryArray = $extended ? array('extended' => 'true') : array();
+    // Return API response.
+    return $this->getData('user', 'view', $username, $queryArray);
+  }
 }
